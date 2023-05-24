42d0a210e304cf91dc04b7f15736175886809b6e
Kirill Bobrov <k1r8r0wn@gmail.com>
2023-05-24 13:34:12 +0300

    Add Ruby's Two Sum solution

---
+++ top_interview_150/ruby/hashmap/1_two_sum.rb
@@ -0,0 +1,61 @@
+# 1. Two Sum
+#
+# Given an array of integers nums and an integer target, return indices of the two numbers such that they add
+# up to target.
+#
+# You may assume that each input would have exactly one solution, and you may not use the same element twice.
+#
+# You can return the answer in any order.
+#
+# Example 1:
+#
+# Input: nums = [2,7,11,15], target = 9
+# Output: [0,1]
+# Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+#
+# Example 2:
+#
+# Input: nums = [3,2,4], target = 6
+# Output: [1,2]
+# Example 3:
+#
+# Input: nums = [3,3], target = 6
+# Output: [0,1]
+#
+# Constraints:
+#
+# 2 <= nums.length <= 104
+# -109 <= nums[i] <= 109
+# -109 <= target <= 109
+# Only one valid answer exists.
+#
+# Follow-up: Can you come up with an algorithm that is less than O(n^2) time complexity?
+
+# Solution
+
+def two_sum(nums, target)
+  nums.each_with_object({}).with_index do |(number, hash), index|
+    comp_index = hash[target - number]
+    return [comp_index, index] if comp_index
+
+    hash[number] = index
+  end
+end
+
+p two_sum([2, 7, 11, 15], 9)
+
+# Notes
+#
+# Algorithm uses One-pass Hash Table.
+#
+# It turns out we can do it in one-pass. While we are iterating and inserting elements into the hash table,
+# we also look back to check if current element's complement already exists in the hash table.
+# If it exists, we have found a solution and return the indices immediately.
+#
+# Complexity Analysis
+#
+# Time complexity: O(n).
+# We traverse the list containing nnn elements only once. Each lookup in the table costs only O(1) time.
+#
+# Space complexity: O(n).
+# The extra space required depends on the number of items stored in the hash table, which stores at most n elements.
